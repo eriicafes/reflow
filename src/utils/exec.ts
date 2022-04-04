6455bd1966c6ca00b7ded9f692f446e4ddac0aee
Eric Afes <eriicafes@gmail.com>
2022-04-04 13:06:46 +0100

    refactor: removed unsued reject param from spawn promise

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -4,7 +4,7 @@ import childProcess from "child_process"
 export const exec = promisify(childProcess.exec)
 
 export const spawn = (command: string) => {
-    return new Promise<void>((resolve, reject) => {
+    return new Promise<void>((resolve) => {
         const [cmd, ...args] = command.split(" ")
 
         const child = childProcess.spawn(cmd, args, {stdio: "inherit"})
